8c340b1b9bde10042b84ecccf9c4eff2defb2a85
Bayu Putra Efendi <91212372+bbaygx@users.noreply.github.com>
2023-07-24 17:09:43 +0700

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const run = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
